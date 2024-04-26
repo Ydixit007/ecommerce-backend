e853f4d0b61cb72280cf0aa4385b960c7ee210ca
Yash Dixit <69184633+Ydixit007@users.noreply.github.com>
2024-04-27 04:52:08 +0530

    Delete dist directory

--- dist/routes/user.js
+++
@@ -1,5 +0,0 @@
-import express from "express";
-import { createNewUserController } from "../controllers/user.js";
-const userRoute = express.Router();
-userRoute.post("/create", createNewUserController);
-export default userRoute;
--- dist/utils/utility-class.js
+++
@@ -1,8 +0,0 @@
-export class ErrorHandler extends Error {
-    constructor(message, statusCode) {
-        super(message);
-        this.message = message;
-        this.statusCode = statusCode;
-        this.statusCode = statusCode;
-    }
-}
