3ad1203edbc89a22bbf81965ef1103e7c576ca44
Yash Dixit <ydixit007@gmail.com>
2024-04-29 04:03:04 +0530

    added product routes for latest products, category, product update and product delete

--- dist/controllers/product.js
+++ dist/controllers/product.js
@@ -2,6 +2,7 @@ import { TryCatch } from "../middlewares/error.js";
 import { Product } from "../models/product.js";
 import { ErrorHandler } from "../utils/utility-class.js";
 import { rm } from "fs";
+// Controller for creating new product
 export const createNewProduct = TryCatch(async (req, res, next) => {
     const { productName, price, discount, sizes, stock, category } = req.body;
     const productImage = req.file;
@@ -30,10 +31,100 @@ export const createNewProduct = TryCatch(async (req, res, next) => {
         message: "product created successfully",
     });
 });
+// Controller for latest products
 export const getLatestProducts = TryCatch(async (req, res, next) => {
     const products = await Product.find({}).sort().limit(5);
     return res.status(200).json({
         success: true,
-        products
+        products,
+    });
+});
+// get all products for admin
+export const getAllProducts = TryCatch(async (req, res, next) => {
+    const products = await Product.find({});
+    return res.status(200).json({
+        success: true,
+        products,
+    });
+});
+// get single product from id.
+export const getSingleProduct = TryCatch(async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    if (!product)
+        return next(new ErrorHandler("product not found", 400));
+    return res.status(200).json({
+        success: true,
+        product,
+    });
+});
+// Controller for getting product categories
+export const getProductCategories = TryCatch(async (req, res, next) => {
+    const category = [
+        {
+            men: ["shirts", "tshirts", "jeans", "trousers", "shoes"],
+            women: ["dresses", "tshirts", "skirts", "jeans", "heels"],
+        },
+    ];
+    // const category = await Product.distinct("category");
+    return res.status(200).json({
+        success: true,
+        category,
+    });
+});
+// Controller for updating an existing product
+export const updateProduct = TryCatch(async (req, res, next) => {
+    const { id } = req.params;
+    const { productName, price, discount, sizes, stock, category } = req.body;
+    const productImage = req.file;
+    const product = await Product.findById(id);
+    // standard checks for null safety.
+    if (!product)
+        return next(new ErrorHandler("product not found", 404));
+    // if request contains new image
+    if (productImage) {
+        // delete old image and replaing with new.
+        rm(product.productImage, () => {
+            console.log("old Image deleted!");
+        });
+        product.productImage = productImage.path;
+    }
+    // check for available data and updating it accordingly.
+    if (productName)
+        product.productName = productName;
+    if (price)
+        product.price = price;
+    if (discount)
+        product.discount = discount;
+    if (sizes)
+        product.sizes = sizes.split(",");
+    if (stock)
+        product.stock = stock.split(",");
+    if (category)
+        product.category = category.toLowerCase();
+    // saving the updated changes.
+    await product.save();
+    return res.status(200).json({
+        success: true,
+        message: "product updated successfully",
+    });
+});
+// Controller for deleting product
+export const deleteProduct = TryCatch(async (req, res, next) => {
+    const { id } = req.params;
+    // checking if id is present in req.
+    if (!id)
+        return next(new ErrorHandler("Invalid id", 404));
+    const product = await Product.findById(id);
+    // checking if product actually exists.
+    if (!product)
+        return next(new ErrorHandler("product not found", 404));
+    rm(product.productImage, () => {
+        console.log("Product image deleted!");
+    });
+    // deleting product from database.
+    await product.deleteOne();
+    return res.status(200).json({
+        success: true,
+        message: "The product has been deleted successfully!"
     });
 });
--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,15 +1,28 @@
 import express from "express";
 import { adminOnly } from "../middlewares/auth.js";
 import { uploadSingleImage } from "../middlewares/multer.js";
-import { createNewProduct, getLatestProducts } from "../controllers/product.js";
+import { createNewProduct, deleteProduct, getAllProducts, getLatestProducts, getProductCategories, updateProduct } from "../controllers/product.js";
 
 const productRoute = express.Router();
 
 // route : /api/v1/product/create
-productRoute.post("/create", uploadSingleImage, createNewProduct);
+productRoute.post("/create",adminOnly, uploadSingleImage, createNewProduct);
 
-// route : /api/v1/user/all
+// route : /api/v1/product/latest
+productRoute.get("/all",adminOnly, getAllProducts);
+
+// route : /api/v1/product/latest
 productRoute.get("/latest", getLatestProducts);
+
+// route : /api/v1/product/category
+productRoute.get("/category", getProductCategories);
+
+// route : /api/v1/product/:id
+productRoute.put("/:id", adminOnly, uploadSingleImage, updateProduct);
+
+// route : /api/v1/product/:id
+productRoute.delete("/:id",adminOnly, deleteProduct);
+
 
 
 export default productRoute;
